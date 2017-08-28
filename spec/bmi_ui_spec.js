9f325e7825ce8b81d22ceed9c6840601b8c6e1c3
Nurlan <nurlan.emiroff@gmail.com>
2017-08-28 17:15:01 +0200

    html fixed

---
+++ spec/bmi_ui_spec.js
@@ -0,0 +1,7 @@
+describe('BMI_UI - index.html', function() {
+  beforeEach(function() {
+    jasmine.getFixtures().fixturesPath = '.';
+    loadFixtures('index.html');
+    $.holdReady(false);
+  });
+});
